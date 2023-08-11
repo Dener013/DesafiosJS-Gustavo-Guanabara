8d4ef0a2934fdd29724b37a8754bf581b3d3e3e1
Dener Nascimento Martins <141853510+Dener013@users.noreply.github.com>
2023-08-11 14:31:15 -0300

    Add files via upload

---
+++ d002/welcome.js
@@ -0,0 +1,5 @@
+function start(){
+    let name = prompt('Whats your name?')
+    let year = prompt('Hello, '+name+'! how old are you?')
+    alert("I just met "+name+" who is "+year+" years old")
+}
